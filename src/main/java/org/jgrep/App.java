fcc31b58703cb685b85dabb3266cd771a564e9ad
yanglikai <yanglikai@yonghui.cn>
2019-03-01 13:44:04 +0800

    Initial commit

---
+++ src/main/java/org/jgrep/App.java
@@ -0,0 +1,13 @@
+package org.jgrep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
